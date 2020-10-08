1bde679368be4d8d3e4799351d510b3d8a63ddc3
Cuong Tran <103cuong@gmail.com>
2020-10-09 02:10:42 +0700

    feat: first version (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import { logger } from 'juno-js';
+import axios, { AxiosRequestConfig } from 'axios';
 
-logger().info('Chao xìn 🇻🇳');
+/**
+ * An HTTP client is implemented from the Axios package.
+ *
+ * @param {string} url A url from a network.
+ * @param {object} options Options to config request.
+ * See more option here: https://github.com/axios/axios
+ */
+const request = (url = '', options: AxiosRequestConfig = {}) => {
+  const {
+    method = 'get', responseType = 'json', data = {}, ...config
+  } = options;
+  return axios({
+    method,
+    url,
+    responseType,
+    data,
+    ...config,
+  });
+};
+
+export { request };
